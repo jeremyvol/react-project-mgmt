b8764cf351886f6d094679ab3b292a2d52fa2390
Jeremy Vollant <j.vollant@gmail.com>
2025-06-21 09:22:40 +0200

    general navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 import ProjectsSidebar from './components/ProjectsSidebar';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  function handleCancelAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onCancelAddProject={handleCancelAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar />
-      <NewProject />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,11 +1,13 @@
 import Input from './Input';
 
-export default function NewProject() {
+export default function NewProject({ onCancelAddProject }) {
   return (
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4'>
         <li>
-          <button className='text-stone-800 hover:text-stone-950'>
+          <button
+            className='text-stone-800 hover:text-stone-950'
+            onClick={onCancelAddProject}>
             Cancel
           </button>
         </li>
